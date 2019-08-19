957e92c5ba3aa90f7800c1f4fdb7c834d40f8c6d
PYyu6 <hpeiyu.yu@mail.utoronto.ca>
2019-08-19 15:53:27 -0400

    local server

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const path = require('path');
 const https = require('https');
 const fs = require('fs');
 const httpsRedirect = require('express-https-redirect');
+const superagent = require('superagent');
+const bodyParser = require('body-parser');
 
 const key = fs.readFileSync(path.join(__dirname, 'selfsigned.key'));
 const cert = fs.readFileSync(path.join(__dirname, 'selfsigned.crt'));
@@ -25,8 +27,31 @@ const port = 3000;
 //   }
 // });
 app.use('/', httpsRedirect(true));
-
+app.use(bodyParser.json());
 app.use('/', express.static(path.join(__dirname, 'static_files')));
+
+app.get('/cors_bypass', (req, res) => {
+  // console.log(JSON.stringify(req.body));
+  // console.log(req.content);
+  // Object.keys(req).forEach((k) => {
+  //   try{
+  //     console.log(k + ',' + JSON.stringify(req[k]) + '\n');
+  //   }catch(error){
+  //     console.log(k);
+  //   }
+    
+  // });
+  console.log(JSON.stringify(req.query));
+  superagent.get(req.query.url)
+            .then((cors_res) => {
+              res.send(cors_res.body);
+              res.end()     
+            })
+            .catch(err => {
+              res.sendStatus(500);
+              res.end();
+            })
+})
 
 // app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 // https.createServer(options, app).listen(port);
