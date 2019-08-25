5d88535b468dbd0a78391175550e06587c0ca528
PYyu6 <hpeiyu.yu@mail.utoronto.ca>
2019-08-25 17:31:13 -0400

    whatever

--- static_files/js/ar_nav.js
+++ static_files/js/ar_nav.js
@@ -125,7 +125,7 @@ const update_ar_display = () => {
         try{
             [state.next_loc, ...state.way_points].map(create_ar_geopose_from_coords)
                                                  .forEach((ele) => {
-                                                     console.log(`CREATING ${ele.getAttribute('lla')}`)
+                                                     console.log(`CREATING ${ele.tagName} at ${ele.getAttribute('lla')}`)
                                                      scene.appendChild(ele);
                                                  });
             // const lat = state.next_loc.lat;
