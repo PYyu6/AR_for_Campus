fa4b11fe24d598bb96c7c3bc230b0cdb099cccc1
PYyu6 <hpeiyu.yu@mail.utoronto.ca>
2019-08-28 15:16:02 -0400

    building code to full name

--- static_files/Resources/code_to_fullname.js
+++ static_files/Resources/code_to_fullname.js
@@ -1,9 +1,31 @@
 
-let file=fopen(getScriptPath("/utcoding_converted"),0);
-if (file != -1){
-    const length = flength(file);
-    const str = fread(file, length);
-    const list = str.split("\n");
-    fclose(file);
+// let file=fopen(getScriptPath("/utcoding_converted"),0);
+// if (file != -1){
+//     const length = flength(file);
+//     const str = fread(file, length);
+//     const list = str.split("\n");
+//     fclose(file);
 
-}
+// }
+const acronymMapping;
+
+$.ajax({
+    url: 'utcoding_coverted.csv',
+    dataType: 'text',
+  }).done(CsvtoArray);
+
+  function CsvtoArray(data){
+        var allRows = data.split(/\r?\n|\r/);
+        // var code2Fullname = [];
+        var code2Fullname = {}
+        for (var oneRow = 0; oneRow < allRows.length; oneRow++) {
+            var rowElement = allRows[oneRow].split(',');
+            let nm = rowElement[0]
+            let acrnym = rowElement[1];
+            code2Fullname.push(rowElement);
+            code2Fullname[acrnym] = nm;
+        }
+        // return code2Fullname;
+        acronymMapping = code2Fullname;
+        console.log(acronymMapping);
+  }
